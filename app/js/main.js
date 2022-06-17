3fd1f9086585146942ccb4d8e24b1b27cfc7be5e
Alena Konovalova <alenakono26@gmail.com>
2022-06-17 16:56:16 +0300

    feat: add style header and section preview

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-    $('body').css('background-color', 'green');
-});
+function ibg() {
+    $.each($('.ibg'), function (index, val) {
+        if($(this).find('img').length>0){
+            $(this).css('background-image', 'url("'+$(this).find('img').attr('src')+'")');
+        }
+    });
+}
+ibg();
